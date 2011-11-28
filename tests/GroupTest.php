2224ac81a95a126f428f3075371432650bd3ed06
Isman Firmansyah <isman.firmansyah@gmail.com>
2011-11-28 14:59:59 +0700

    added groups API code and its testcases

---
+++ tests/GroupTest.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once '../lib/Sixreps/Client.php';
+
+class GroupTest extends PHPUnit_Framework_TestCase {
+
+	public function setUp() {
+		$this->group = $this->getMockBuilder('Sixreps_Api_Group')
+		                    ->disableOriginalConstructor()
+		                    ->getMock();
+	}
+
+	public function testGetGroup() {
+		$result = new StdClass;
+    	$result->title = 'Mock Group';
+
+		$this->group->expects($this->any())
+		            ->method('getGroup')
+		            ->with(1)
+		            ->will($this->returnValue($result));
+
+		$this->group->getGroup(1);
+	}
+
+}
